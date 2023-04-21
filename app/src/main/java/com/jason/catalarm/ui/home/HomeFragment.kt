6b3ccf1d7c028e94f3555637e805b1210d637b4e
jasonguo0606 <jasonguo0606@aliyun.com>
2023-04-21 10:31:25 +0800

    Initial commit

---
+++ app/src/main/java/com/jason/catalarm/ui/home/HomeFragment.kt
@@ -0,0 +1,66 @@
+package com.jason.catalarm.ui.home
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.jason.catalarm.databinding.FragmentHomeBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class HomeFragment : Fragment() {
+
+    private val handler: Handler = Handler()
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            val currentTime = Calendar.getInstance().time
+            textView.text =
+                android.text.format.DateFormat.format("yyyy-MM-dd HH:mm:ss", currentTime)
+        }
+        startClock()
+        return root
+    }
+
+    private fun startClock() {
+        handler.post(object : Runnable {
+            override fun run() {
+                val dateFormat = SimpleDateFormat("hh:mm:ss a", Locale.US)
+                val textView: TextView = binding.textHome
+                textView.text = dateFormat.format(Date())
+                handler.postDelayed(this, 1000)
+            }
+        })
+    }
+
+    override fun onStop() {
+        super.onStop()
+        handler.removeCallbacksAndMessages(null)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
